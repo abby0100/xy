e7386cba8d40f5d7bb462e13c4b19c40f96612b8
abby0100 <yuan.xiong@intel.com>
2017-04-08 18:10:24 +0800

    add dynamic route and base converter.

---
+++ python-web/4dynamic-route/dynamic-rount.py
@@ -0,0 +1,64 @@
+# coding = utf-8
+
+from flask import Flask
+app = Flask(__name__)
+
+# 1. config #
+#app.config['DEBUG'] = False
+
+#app.config.update(
+#        DEBUG=True,
+#        SECRET_KEY='...'
+#    )
+
+app.config.from_object('settings')
+import settings
+app.config.from_object(settings)
+
+app.config.from_pyfile('settings.py', silent=True)
+
+# 2. logging #
+import logging
+logging.basicConfig(
+	level=logging.DEBUG,
+	format='%(asctime)s %(name)-8s %(levelname)-8s %(message)s',
+        datefmt='%m-%d %H:%M:%S',
+	filename='myapp.log',
+	filemode='w')
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-8s %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+
+logging.getLogger('').addHandler(console)
+log = logging.getLogger('xy')
+
+# 3. debug #
+#app.debug = True
+#app.run()
+#app.run(debug=True)
+
+
+# 4. dynamic route #
+@app.route('/item/<id>/')
+def item(id):
+    return 'Item:{}'.format(id)
+
+@app.route('/<any(a, b):page_name>/')
+def any():
+    return 'any'
+
+#
+# 0. hello world #
+#
+@app.route('/')
+def hello_world():
+    log.debug('hello world')
+    return 'Hello Python Web!'
+
+if __name__ == '__main__':
+    log.debug('############## main #################')
+    app.run(host='localhost', port=1027, debug=True)
+
+
